4f1502ea638a7ac4f61016edeb7b7977e071a806
abhiramkrishna66 <147504110+abhiramkrishna66@users.noreply.github.com>
2025-02-12 11:51:01 +0530

    Create RelationalOp.java

---
+++ RelationalOp.java
@@ -0,0 +1,18 @@
+public class RelationalOp
+{
+	public static void main(String[] args) {
+	    int a=20;
+	    int b=10;
+	    int c=14;
+	    if(a>b && a>c){
+	        System.out.println("A is Greatest");
+	    }
+	    else if(b>a && b>c){
+	        System.out.println("B is Greatest");
+	    }
+	    else{
+	        System.out.println("C is greatest");
+	    }
+	
+	}
+}
