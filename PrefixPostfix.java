ee75c86ba56ef09cf503744253fd05c5b5bb214a
abhiramkrishna66 <147504110+abhiramkrishna66@users.noreply.github.com>
2025-02-12 14:33:11 +0530

    Create PrefixPostfix.java

---
+++ PrefixPostfix.java
@@ -0,0 +1,9 @@
+public class public class PrefixPostfix{
+    public static void main(String[] args){
+        int x=10;
+        //int v= --x + x++ + ++x + --x + x++;
+        int z=x++ + ++x + ++x + x++ + ++x;
+        
+        System.out.println(z);
+    }
+}
