f791ec5aad30dd2fdf3a7e7f7943a5adbcea4722
abhiramkrishna66 <147504110+abhiramkrishna66@users.noreply.github.com>
2025-02-12 12:41:14 +0530

    Create BitwiseOp.java

---
+++ BitwiseOp.java
@@ -0,0 +1,9 @@
+public class BitwiseOp
+{
+	public static void main(String[] args) {
+	    int a=492;
+	    int b=333;
+	    System.out.println("Bitwise OR"+ " "+(a|b));
+	    System.out.println("Bitwise AND"+ " " +(a&b));
+	    System.out.print("Bitwise XOR"+ " "+(a^b));
+	}}
