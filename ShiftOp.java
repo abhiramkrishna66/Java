102f37aff9baeb084e69843f2ba15b87afa423a3
abhiramkrishna66 <147504110+abhiramkrishna66@users.noreply.github.com>
2025-02-12 14:04:32 +0530

    Create ShiftOp.java

---
+++ ShiftOp.java
@@ -0,0 +1,11 @@
+*******************************************************************************/
+public class ShiftOp{
+    public static void main(String[] args){
+        int a = 10;
+        int b=a<<2;
+        int c=b>>2;
+        System.out.println("Left Shift: "+b);
+        System.out.println("RightShift: "+c);
+        
+    }
+}
