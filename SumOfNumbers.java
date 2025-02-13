511d65683a12ee33208df75d4f92aa10142f1553
abhiramkrishna66 <147504110+abhiramkrishna66@users.noreply.github.com>
2025-02-13 10:46:23 +0530

    Sum of odd digits in a number

---
+++ SumOfNumbers.java
@@ -0,0 +1,14 @@
+public class SumOfNumbers{
+    public static void main(String[] args){
+        int number=123456;
+        int sum=0;
+        String str=Integer.toString(number);
+        for(int i=0;i<=str.length()-1;i=i+2){
+            char num=str.charAt(i);
+             int num2 = Integer.parseInt(String.valueOf(num));
+            sum+=num2;
+            
+        }
+        System.out.println(sum);
+    }
+}
