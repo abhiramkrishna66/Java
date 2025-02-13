2b283454e9bf48dae872c4000e928da43a9df27a
abhiramkrishna66 <147504110+abhiramkrishna66@users.noreply.github.com>
2025-02-13 14:07:16 +0530

    Create TriangleofNum2.java

---
+++ TriangleofNum2.java
@@ -0,0 +1,17 @@
+public class TriangleofNum2{
+    public static void main(String[] args){
+        int sum=0;
+        int n=5;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                sum+=1;
+                System.out.print(sum+" ");
+               
+            }
+            System.out.println(" ");
+           
+
+}
+}
+        }
+    
