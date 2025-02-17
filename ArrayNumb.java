3a3ceddb7a95b092843035356075c3eccb51fdf4
abhiramkrishna66 <147504110+abhiramkrishna66@users.noreply.github.com>
2025-02-17 14:40:34 +0530

    Create ArrayNumb.java

---
+++ ArrayNumb.java
@@ -0,0 +1,19 @@
+public class ArrayNumb{
+    public static void main(String[] args){
+        int arr[]={1,2,3,4,5,6};
+        int n=3;
+       int sum=0;
+ 
+           for(int i=n-1;i>=0;i--){
+               System.out.print(arr[i]);
+               
+           }
+           for(int i=arr.length-1;i>=n;i--){
+               System.out.print(arr[i]);
+              
+           
+       }
+            }
+            
+        }
+    
