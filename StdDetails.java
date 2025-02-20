e49c5cc1b71414e90734dbc03a609164e2fb732d
abhiramkrishna66 <147504110+abhiramkrishna66@users.noreply.github.com>
2025-02-20 11:39:49 +0530

    Update StdDetails.java

--- StdDetails.java
+++ StdDetails.java
@@ -1,17 +1,26 @@
 import java.util.Scanner;
-class Student{
-   void get(String n,int r){
-       System.out.println("name:"+n);
-       System.out.println("reg:"+r);
-   }}
-   public class StdDetails{
-   public static void main(String[] args){
-   Scanner sc=new Scanner(System.in);
-   System.out.print("Enter the Student's Name:");
-   String name=sc.nextLine();
-   System.out.print("Enter the Register Number:");
-   int reg=sc.nextInt();
-       Student obj=new Student();
-       obj.get(name,reg);
-   }
+class Student {
+	void get(String arr[],int arr1[]) {
+	    for(int i=0;i<5;i++){
+		System.out.println("name:"+arr[i]);
+		System.out.println("reg:"+arr1[i]);
+	}
+}
+}
+public class StdDetails{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String arr[]=new String[5];
+		int arr1[]=new int[5]; 
+		for (int i=0;i<5;i++){
+		System.out.print("Enter the Student's Name:");
+		arr[i]=sc.nextLine();
+		System.out.print("Enter the Register Number:");
+		arr1[i]=sc.nextInt();
+		sc.nextLine();
+		}
+		
+		Student obj=new Student();
+		obj.get(arr,arr1);
+	}
 }
